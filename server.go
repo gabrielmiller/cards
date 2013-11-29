e71783c4e52c91d54eb8fcf3bd4eec50d4ebcb57
L. Gabriel Miller <gmiller2007@gmail.com>
2013-11-28 21:49:44 -0500

    Adds struct user Intercepts requests to /user and console logs the parsed user struct

--- server.go
+++ server.go
@@ -3,15 +3,42 @@ package main
 import(
     "fmt"
     "net/http"
+    "encoding/hex"
+    "encoding/json"
+    "crypto/sha512"
+    //"database/sql"
+    //"github.com/bmizerany/pq" //_
 )
+
+type User struct {
+    UserName string
+    UserEmail string
+    UserPassword string
+    UserHash string
+    UserRemember bool
+    UserTOS bool
+}
 
 func index(w http.ResponseWriter, req *http.Request){
     http.ServeFile(w, req, "./index.html")
 }
 
 func user(w http.ResponseWriter, req *http.Request){
-    fmt.Println("user hit")
-    fmt.Println(req)
+    fmt.Println("-------------")
+    fmt.Println(req.Method)
+    user := new(User)
+    json.NewDecoder(req.Body).Decode(user)
+    hash := sha512.New()
+    hash.Write([]byte("this is a test"))
+    hashArray := make([]byte, hash.Size())
+    hash.Sum(hashArray[:0])
+    user.UserHash = hex.EncodeToString(hashArray)
+    fmt.Println(user.UserName)
+    fmt.Println(user.UserEmail)
+    fmt.Println(user.UserPassword)
+    fmt.Println(user.UserHash)
+    fmt.Println(user.UserRemember)
+    fmt.Println(user.UserTOS)
 }
 
 func serveFile(pattern string, filename string) {
