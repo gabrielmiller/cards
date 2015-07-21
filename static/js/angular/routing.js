16a293407ac683df781f96b341468db7ba227f0d
L Gabriel Miller <gmiller2007@gmail.com>
2015-07-20 21:55:43 -0400

    Loads angular and ui router from cdns. Starts implementing ui router. Removes unused cruft.

---
+++ static/js/angular/routing.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict'
+
+    angular
+        .module('cardsApp')
+        .config(cardsConfig);
+
+    cardsConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function cardsConfig($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/home");
+
+        $stateProvider
+            .state("about", {
+                controller: "aboutCtrl",
+                controllerAs: "about",
+                templateUrl: "partials/about.html",
+                url: "/about"
+            })
+            .state("chat", {
+                controller: "chatCtrl",
+                controllerAs: "chat",
+                templateUrl: "partials/chat.html",
+                url: "/chat"
+            })
+            .state("home", {
+                controller: "homeCtrl",
+                controllerAs: "home",
+                templateUrl: "partials/home.html",
+                url: "/home"
+            })
+            .state("signup", {
+                controller: "signupCtrl",
+                controllerAs: "signup",
+                templateUrl: "partials/signup.html",
+                url: "/signup"
+            });
+    }
+})();
