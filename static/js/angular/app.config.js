8c28260ae825d099cfe790e428d701551c994671
L Gabriel Miller <gmiller2007@gmail.com>
2015-07-31 22:21:05 -0400

    Makes angular client use html5Mode routing.

--- static/js/angular/app.config.js
+++ static/js/angular/app.config.js
@@ -5,10 +5,12 @@
         .module('cardsApp')
         .config(cardsConfig);
 
-    function cardsConfig($httpProvider) {
+    function cardsConfig($httpProvider, $locationProvider) {
 
         // Sets an interceptor so that requests specify a token
         $httpProvider.interceptors.push('jwtAuthenticationInterceptor')
+
+        $locationProvider.html5Mode(true);
 
 
     }
